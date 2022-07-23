facac456b475c6a5ce340aff539695ec3452c3cc
Vadim Turanosoff <turanosoff@gmail.com>
2022-07-23 14:31:02 +0300

    new data

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
 
     public int calculate(int cost) {
-        int amount = 5 ;
+        int amount = 100;
         int result = (cost / amount);
         System.out.println("вам начислено" + result);
         return result;
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        int cost = 100;
+        int cost = 10_000;
         BonusMilesService service = new BonusMilesService();
-        int price = 100;
+        int price = 10_000;
         int miles = service.calculate(price);
         System.out.println(miles);
 
